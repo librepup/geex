c21c5a22cb1061cb588d0d2b1f251256d478fa1f
nixpup <zweitlaender@gmail.com>
2026-02-22 02:27:00 +0100

    Fixed Extra Package Insertion Error in Geex Installer

--- geex.sh
+++ geex.sh
@@ -2196,7 +2196,7 @@ addCustomPackageHook() {
         export pkgListWasConfirmed=0
     fi
     if [ "$pkgListWasConfirmed" == 1 ]; then
-        export extraPackageList="$(echo -e "$finalCustomPkgList")"
+        export extraPackageList="$(echo -e "$finalCustomPkgList\"")"
         echo -e "[ Status ]: List was Confirmed, full List:\n'$extraPackageList'"
         if [[ "$extraPackageList" != "" ]] || [[ -n "$extraPackageList" ]]; then
             export extraPackageListInsertable="$(echo -e "\nUWUUWUUWUUWUUWUUWUUWUUWUUWUUWUUWUUWUUWUUWUUWUUWUUWUUWUUWUUWUUWUUWUUWUUWUUWUUWUUWUUWUUWU\"$extraPackageList" | sed "s/ /\"\n                             \"/g" | sed "s/UWU/ /g")"
