8b84edbbedad9c9d2eb32aaebe9de5f83c91be8e
librepup <zweitlaender@gmail.com>
2026-03-03 01:08:42 +0100

    Added Scripts and Updated XMonad and NaitreHUD Config

--- files/scripts/waybar.sh
+++ files/scripts/waybar.sh
@@ -1,50 +1,7 @@
 #!/usr/bin/env bash
 
-# Case Config File
-case "$1" in
-    config|c)
-        export configPath="$HOME/.config/waybar"
-        export configFile="$configPath/config.jsonc"
-        ;;
-    geex|g)
-        export configPath="$HOME/Geex/files/config/waybar"
-        export configFile="$HOME/Geex/files/config/waybar/config.jsonc"
-        ;;
-    *|"")
-        export configPath="$HOME/Geex/files/config/waybar"
-        export configFile="$HOME/Geex/files/config/waybar/config.jsonc"
-        ;;
-esac
-
-# Case Theme
-case "$2" in
-    guix|g)
-        export waybarTheme="guix.css"
-        ;;
-    jungle|j)
-        export waybarTheme="jungle.css"
-        ;;
-    default|d)
-        export waybarTheme="style.css"
-        ;;
-    *|"")
-        export waybarTheme="jungle.css"
-        ;;
-esac
-
-# If Waybar already Running, Kill - else, Start
 if ps -e | grep "[w]aybar"; then
     pkill waybar
 else
-    if [ -d ~/.config/waybar ] || [ -d ~/Geex/files/config/waybar ]; then
-        echo "Found Config Directories"
-    else
-        echo "Config Directories Missing"
-    fi
-    waybar --config $configFile --style $configPath/$waybarTheme &
+    waybar --config ~/.config/waybar/config.jsonc --style ~/.config/waybar/jungle.css &
 fi
-
-# Unset Exported Variables
-unset waybarTheme
-unset configPath
-unset configFile
